beef175b62562e1c0fa28b906fec1de127ea9eea
Ha Nguyen <nguyenthingocha2@gmail.com>
2020-02-19 21:39:57 -0500

    working id tags

--- script.js
+++ script.js
@@ -2,29 +2,32 @@
 var apiKey = "cc4b8a3876056cbd6e597915655fb722";
 var cityId = "";
 
-  $("#searchFormButton").on("submit", function(event) {
-    event.preventDefault();
-       cityId = $("#search").val();
-       console.log(cityId);
-       // URL we need to query the database
-       var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityId + "&APPID=" + apiKey;
+$("#searchFormButton").on("submit", function(event) {
+  event.preventDefault();
+    cityId = $("#search").val();
+    console.log(cityId);
+    // URL we need to query the database
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityId + "&APPID=" + apiKey;
 
-   
-       $.ajax({
-        //$.curl({
-        url: queryURL,
-        method: 'GET'
-      }).then(function(response) {
-    console.log(response);
-    console.log(response.city.name)
+
+  $.ajax({
+    //$.curl({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+  console.log(response);
+  console.log(response.city.name)
+
+  var coords = response.coords
 
     var cityName = response.city.name
     var temperature = ((Math.floor(response.list["0"].main.temp - 273.15) * 1.80 + 32) + " degrees")
     var humidity = response.list["0"].main.humidity
     var windspeed = response.list["0"].wind.speed
     var uvIndex = response.city.name
-    var currentTime = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    //varuvIndexBig`
+    var currentTime = $("#currentDay").text(moment().format("MM/DD/YYYY"));
+    
+    //getUVIndex(coords);
 
     var mainDiv1 = document.createElement("div");
     var mainDiv1CityName = mainDiv1.textContent = cityName;
@@ -42,12 +45,41 @@ var cityId = "";
     var mainDiv4Windspeed = mainDiv4.textContent = windspeed + " MPH";
         $("#windBig").append(mainDiv4Windspeed);
 
-// $("#currentDay").text(moment().format("$("1"));
+    
 
+      
+  })
 
-      })
+  });
 
-    });
-    function getUVIndex() {
-
+  // function getUVIndex(coords) {
+  //     console.log(coords)
+  //     var lat = coords.lat;
+  //     var lon = coords.lon;
+  //     var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid=9eb05d381db07ca6a0d36e08a08087f1&lat=" + lat + "&lon=" + lon;
+    
+  //     $.ajax({
+  //       url: queryUV,
+  //       method: "GET"
+  //       
+  //       // Open Weather returns a JSON form
+    
+  //     }).then(function(response) {
+  //   var valueUV = response.value;
+  //   var el = $("<span>").text(valueUV);
+    
+    if (valueUV < 3) {
+      el.addClass("heatIndexGreen")
+    } else if (valueUV < 7) {
+      el.addClass("heatIndexYellow")
+    } else {
+      el.addClass("heatIndexRed")
     }
+    
+  //   $("#mainUV").append(el)
+    
+  //     })
+  //       
+  //       // Go through the JSON form and pull data from the objects
+    
+  //   }
